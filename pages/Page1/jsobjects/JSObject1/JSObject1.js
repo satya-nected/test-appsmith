bd63410727dce7960d12051177146bbddca9f64a
satya <satya@nected.ai>
2025-09-22 10:28:54 +0000

    .

---
+++ pages/Page1/jsobjects/JSObject1/JSObject1.js
@@ -0,0 +1,11 @@
+export default {
+	myVar1: [],
+	myVar2: {},
+	myFun1 () {
+		return {'a':1}
+	},
+	async myFun2 () {
+		//	use async-await or promises
+		//	await storeValue('varName', 'hello world')
+	}
+}
